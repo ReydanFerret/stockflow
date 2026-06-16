dfd924fbb5a136bb0596da9af77244f5c7f42ef2
ReydanFerret <krolcyta@gmail.com>
2026-06-16 01:25:36 +0000

    feature(login):Se añadieron comentarios a la verificacion de roles, ademas de la verificacion para el usuario y contraseña

--- JS/login.js
+++ JS/login.js
@@ -4,13 +4,35 @@ loginForm.addEventListener("submit", function(e) {
 
     e.preventDefault();
 
+    const usuario = document.getElementById("nombreUsuario").value;
+    const contraseña = document.getElementById("contraseñaUsuario").value;
     const rol = document.getElementById("rolUsuario").value;
 
-    if (rol === "") {
-        alert("Seleccione un rol");
+    // Validar cédula
+    if (usuario.length !== 8) {
+        alert("La cédula debe tener exactamente 8 dígitos.");
         return;
     }
 
+    // Validar que sean solo números
+    if (isNaN(usuario)) {
+        alert("La cédula solo puede contener números.");
+        return;
+    }
+
+    // Validar contraseña
+    if (contraseña.trim() === "") { // Esta linea elimina espacios para que no tome espacios vacios como contraseña valida
+        alert("Debe ingresar una contraseña.");
+        return;
+    }
+
+    // Validar rol
+    if (rol === "") {
+        alert("Seleccione un rol.");
+        return;
+    }
+
+    // Redirección según rol
     if (rol === "admin") {
         window.location.href = "dashboard.html";
     }
